83d71545ed9879af3fe7aa8f75878e53e7b347c1
Moneshwar-R <moneshwar.r2024it@sece.ac.in>
2025-12-05 15:40:28 +0530

    Add files via upload

---
+++ JavaScript Programs/callback.js
@@ -0,0 +1,10 @@
+function hello(show){
+    if(true){
+        show("Hello World")
+    }
+}
+console.log("Start of the Program")
+hello(display)
+function display(msg){
+    console.log("The Message is: "+msg)
+}
